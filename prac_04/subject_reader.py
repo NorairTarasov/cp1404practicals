2547f42505045afdcadfc40b9f82b8185fe9de3b
NorairTarasov <tarasov.norik@gmail.com>
2023-08-06 21:18:12 +0800

    Subject Reader fixed

---
+++ prac_04/subject_reader.py
@@ -0,0 +1,36 @@
+"""
+CP1404/CP5632 Practical
+Data file -> lists program
+"""
+
+FILENAME = "subject_data.txt"
+
+
+def main():
+    data = get_data()
+    display_data(data)
+
+
+def get_data():
+    data = []
+    input_file = open(FILENAME)
+    for line in input_file:
+        print(line)  # See what a line looks like
+        print(repr(line))  # See what a line really looks like
+        line = line.strip()  # Remove the \n
+        parts = line.split(',')  # Separate the data into its parts
+        print(parts)  # See what the parts look like (notice the integer is a string)
+        parts[2] = int(parts[2])  # Make the number an integer (ignore PyCharm's warning)
+        print(parts)  # See if that worked
+        print("----------")
+        data.append(parts)
+    input_file.close()
+    return data
+
+
+def display_data(data):
+    for i in data:
+        print(f"{i[0]} is taught by {i[1]:12} and has {i[2]:3} students")
+
+
+main()
